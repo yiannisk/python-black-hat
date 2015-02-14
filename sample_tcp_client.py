686cd1fc5250be1fdb6445db9c9f4e36141caac6
yiannisk <ikaradimas@hotmail.com>
2015-02-14 19:07:15 +0200

    Some initial scripts.

---
+++ sample_tcp_client.py
@@ -0,0 +1,13 @@
+import socket
+
+target_host = "127.0.0.1"
+target_port = 9999
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((target_host, target_port))
+
+client.send("GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+
+response = client.recv(4096)
+
+print response
